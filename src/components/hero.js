2bd0d97de4d54f2b941451c85817d166b433357c
buttreygoodness <jeff@readypress.com>
2021-03-09 17:08:05 -0700

    Updates

--- src/components/hero.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import CarouselCss from './carousel-css'
-
-export default ({ posts }) => (
-  <section className="hero is-medium is-light is-bold">
-    <CarouselCss posts={posts} />
-  </section>
-)
